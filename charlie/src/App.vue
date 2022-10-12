742a1ada42e348fe2cdaa811a949361aa28a309a
Ivijan Hadija <ivijan.hadija@gmail.com>
2022-10-12 22:43:43 +0200

    update redirection process

--- charlie/src/App.vue
+++ charlie/src/App.vue
@@ -1,22 +1,32 @@
 <template>
-  <div v-if="loggedUser.role === 'user'">
-    <nav class="navbar navbar-expand navbar-dark bg-dark">
-      <div class="navbar-nav">
-        <a class="nav-item nav-link" :href="alphaUrl + '/logout'">Logout</a>
-      </div>
-    </nav>
-    <div class="card mt-4">
-      <h4 class="card-header">Congrats, you're logged in</h4>
-      <div class="card-body">
-        <ul>
-          <li>User: {{ loggedUser.firstName }} {{ loggedUser.lastName }}</li>
-          <li>Role: {{ loggedUser.role }}</li>
-        </ul>
+  <div>
+    <div v-if="loggedUser?.role === 'user'">
+      <nav class="navbar navbar-expand navbar-dark bg-dark">
+        <h1 class="navbar-brand">User Role Dashboard</h1>
+        <div class="navbar-nav">
+          <a class="nav-item nav-link" :href="alphaUrl + '/logout'">Logout</a>
+        </div>
+      </nav>
+      <div class="card mt-4">
+        <h4 class="card-header">Congrats, you're logged in</h4>
+        <div class="card-body">
+          <ul>
+            <li>User: {{ loggedUser.firstName }} {{ loggedUser.lastName }}</li>
+            <li>Role: {{ loggedUser.role }}</li>
+          </ul>
+        </div>
       </div>
     </div>
-  </div>
-  <div style="display: none">
-    <iframe :src="alphaUrl"></iframe>
+    <div v-if="showRedirectMessage">
+      <div class="card mt-4">
+        <h4 class="card-header">
+          Redirecting due to wrong user role in {{ this.timerCount }} seconds.
+        </h4>
+      </div>
+    </div>
+    <div style="display: none">
+      <iframe :src="alphaUrl"></iframe>
+    </div>
   </div>
 </template>
 
@@ -28,6 +38,8 @@ export default {
     return {
       alphaUrl: process.env.VUE_APP_ALPHA_URL,
       loggedUser: {},
+      showRedirectMessage: false,
+      timerCount: 5,
     };
   },
   mounted() {
@@ -40,8 +52,20 @@ export default {
       if (event.origin !== process.env.VUE_APP_ALPHA_URL) return;
       this.loggedUser = event.data.user;
       if (this.loggedUser?.role !== "user") {
-        window.location.href =
-          process.env.VUE_APP_ALPHA_URL + "/role?role=user";
+        this.showRedirectMessage = true;
+        this.countdown();
+        setTimeout(() => {
+          window.location.href =
+            process.env.VUE_APP_ALPHA_URL + "/role?role=user";
+        }, this.timerCount * 1000);
+      }
+    },
+    countdown() {
+      if (this.timerCount > 0) {
+        setTimeout(() => {
+          this.timerCount--;
+          this.countdown();
+        }, 1000);
       }
     },
   },
